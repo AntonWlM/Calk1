590fc40625ab794733eb348a3099bbb263356c96
AntonWlM <146090601+AntonWlM@users.noreply.github.com>
2023-09-27 14:48:09 +0300

    Add files via upload

--- Calculator6.java
+++ Calculator6.java
@@ -21,8 +21,7 @@ public class Calculator6 {
             int a3 = parseStrToInt(a);
             int b3 = parseStrToInt(b);
             int x;
-            {
-                if ((0 <= a3) && (a3 <= 10)) {
+            {if ((0 <= a3) && (a3 <= 10)) {
                     if ((0 <= b3) && (b3 <= 10)) {
                         switch (oper) {
                             case '+':
@@ -41,8 +40,10 @@ public class Calculator6 {
                                 x = a3 / b3;
                                 System.out.println(x);
                                 break;
-                            default:
-                                ;
+                            default: {
+                                throw new Exception("throws Exception //т.к.строка не является математической операцией");
+                            }
+
                         }
 
                     } else throw new Exception("throws Exception //т.к. введенные числа вне диапазона от 0 до 10");
@@ -54,6 +55,7 @@ public class Calculator6 {
             int a3 = parseRimToArab(a);
             int b3 = parseRimToArab(b);
             int x = 0;
+            if (a3 == 0 || b3 == 0) {throw new Exception("throws Exception //т.к. используются одновременно разные системы счисления");}
             switch (oper) {
                 case '+':
                     x = a3 + b3;
@@ -90,7 +92,7 @@ public class Calculator6 {
         }
     }
 
-    static int parseRimToArab(String str) {
+    static int parseRimToArab(String str) throws Exception {
         int c3 = 0;
 
         if (str.equals("I")) {
@@ -123,8 +125,8 @@ public class Calculator6 {
         } else if (str.equals("X")) {
             c3 = 10;
             return c3;
-        } else ;
-        return c3;
+        } else throw new Exception("throws Exception //т.к. введенные числа вне диапазона от I до X");
+
     }
 
     static void parseArabToRim(int q) throws Exception {
@@ -162,8 +164,116 @@ public class Calculator6 {
             case (10):
                 System.out.println("X");
             case (0) :
-                System.out.println(0);
+                System.out.println("Результат вне диапазона возможных значений римских цифр");
                 break;
+            case (11):
+                System.out.println("XI");
+                break;
+            case (12):
+                System.out.println("XII");
+                break;
+            case (13):
+                System.out.println("XIII");
+                break;
+            case (14):
+                System.out.println("XIV");
+                break;
+            case (15):
+                System.out.println("XV");
+                break;
+            case (16):
+                System.out.println("XVI");
+                break;
+            case (17):
+                System.out.println("XVII");
+                break;
+            case (18):
+                System.out.println("XVIII");
+                break;
+            case (19):
+                System.out.println("XIX");
+                break;
+            case (20):
+                System.out.println("XX");
+            case (21):
+                System.out.println("XXI");
+                break;
+            case (24):
+                System.out.println("XXIV");
+                break;
+            case (25):
+                System.out.println("XXV");
+                break;
+            case (27):
+                System.out.println("XVII");
+                break;
+            case (28):
+                System.out.println("XXVIII");
+                break;
+            case (30):
+                System.out.println("XXX");
+                break;
+            case (32):
+                System.out.println("XXXII");
+                break;
+            case (34):
+                System.out.println("XXXIV");
+                break;
+            case (35):
+                System.out.println("XXXV");
+                break;
+            case (36):
+                System.out.println("XXXVI");
+                break;
+            case (40):
+                System.out.println("XL");
+            case (42):
+                System.out.println("IXL");
+                break;
+            case (45):
+                System.out.println("XLV");
+                break;
+            case (48):
+                System.out.println("XLVIII");
+                break;
+            case (49):
+                System.out.println("XLIX");
+                break;
+            case (50):
+                System.out.println("L");
+                break;
+            case (54):
+                System.out.println("LIV");
+                break;
+            case (56):
+                System.out.println("LVI");
+                break;
+            case (60):
+                System.out.println("LX");
+                break;
+            case (63):
+                System.out.println("LXIII");
+                break;
+            case (64):
+                System.out.println("LXIV");
+            case (70):
+                System.out.println("LXX");
+                break;
+            case (72):
+                System.out.println("LXXII");
+                break;
+            case (80):
+                System.out.println("LXXX");
+                break;
+            case (81):
+                System.out.println("LXXXI");
+                break;
+            case (90):
+                System.out.println("XC");
+                break;
+            case (100):
+                System.out.println("C");
+
             default: {
 
                         System.out.println(q); }
